910bd0818f7db8a8b7ce1e9d20b4c4d986ec19c2
Dwayne <dwayne.dsouza@gmail.com>
2024-09-23 23:13:16 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
 export default function App() {
+
+
+  useEffect(() => {
+      async function loadNotification() {
+        Notifications.requestPermissionsAsync();
+       const token= (await Notifications.getDevicePushTokenAsync()).data;
+       console.log("Push token:",token);
+      }
+      loadNotification();
+  });
+
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
